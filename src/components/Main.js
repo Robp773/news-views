5f0f472b3921fcceaef0ebcecebdb2fdc4e87a85
Your Name <robpeterman573@gmail.com>
2018-02-05 17:41:04 -0500

    Initial commit

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import './Main.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Main extends React.Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <div>Basic Test</div>
+        )
+    }
+}
