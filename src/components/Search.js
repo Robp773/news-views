0137b7dc276970480eb4500a05f18a89657a5c7d
Robp773 <robpeterman573@gmail.com>
2018-08-21 16:05:13 -0400

    changed get request from search to use newsapi + configured searchQuery function to eventually take in user defined search parameters

--- src/components/Search.js
+++ src/components/Search.js
@@ -3,101 +3,101 @@ import './Search.css'
 import { connect } from 'react-redux'
 import { populateState, loading } from '../actions'
 import Freq from 'wordfrequenter'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-
+import NewsAPI from 'newsapi'
 export class Search extends React.Component {
   super (props) {
     constructor(props)
   }
 
-  searchSent (query, e) {
+  testQuery (query, sortBy, pageSize, newsSourceArray, e) {
     e.preventDefault()
     if (query === '') {
       return alert('Please enter a search term.')
     }
+
     this.props.dispatch(loading())
+
     if (this.props.setInitialSearch) {
       this.props.setInitialSearch()
     }
 
-    // Search the three news networks for headlines on the searched term
-    // const newsapi = new NewsAPI('a8bbe0f664d741539f4eeb966fa99339')
-    let sourceArray = ['msnbc', 'cnn', 'fox-news']
-    let siteArray = ['msnbc', 'cnn', 'fox']
-    // loop through this entire sequence once for each news source
-    for (let j = 0; j < sourceArray.length; j++) {
-      let combinedHeadlines = []
-      let titleAndUrl = []
-      let descriptionArray = []
-      let url = `https://newsapi.org/v2/everything?apiKey=a8bbe0f664d741539f4eeb966fa99339&q=${query}&sources=${
-        sourceArray[j]
-      }&sortBy=relevancy&language=en`
-      var req = new Request(url)
-      fetch(req)
-        .then(res => {
-          return res.json()
-        })
-        .then(response => {
-          let length
-          if (response.articles.length >= 5) {
-            length = 5
-          } else if (response.articles.length < 5) {
-            length = response.articles.length
-          }
-          for (let i = 0; i < length; i++) {
-            descriptionArray.push(response.articles[i].description)
-            titleAndUrl.push({
-              headlineText: response.articles[i].title,
-              description: response.articles[i].description,
-              url: response.articles[i].url
-            })
-            combinedHeadlines.push(response.articles[i].title)
-          }
+    const newsapi = new NewsAPI('a8bbe0f664d741539f4eeb966fa99339')
+    let sourceArray = ['sourceOne', 'sourceTwo', 'sourceThree']
 
-          let joinedDescriptions = descriptionArray.join('').split(' ')
-          const wordCloudArray = new Freq(joinedDescriptions)
-          let result = wordCloudArray.list()
-          let initializedArray = []
-          for (let b = 0; b < result.length; b++) {
-            initializedArray.push({
-              text: result[b].word,
-              value: result[b].count
-            })
-          }
-          let wordFilter = [
-            'Fox',
-            'MSNBC',
-            'fox',
-            'cnn',
-            'CNN',
-            '(CNN)',
-            'from',
-            'with',
-            'a',
-            'A',
-            'the',
-            'who',
-            'of',
-            'to',
-            'and',
-            'in',
-            'on',
-            'for'
-          ]
-          var filteredArray = initializedArray.filter(function (el) {
-            return wordFilter.indexOf(el.text) <= -1
+    for (let i = 0; i < sourceArray.length; i++) {
+      newsapi.v2.everything({
+        sources: newsSourceArray[i],
+        q: query,
+        sortBy: sortBy,
+        pageSize: pageSize,
+        language: 'en',
+      }).then(response => {
+        console.log(response)
+
+        let titleAndUrl = []
+        let descriptionArray = []
+
+        for (let i = 0; i < pageSize; i++) {
+          // used for wordcloud frequency counting later- basically a large pile of words
+          //  taken from headline descriptions
+          descriptionArray.push(response.articles[i].description)
+          // combinedHeadlines.push(response.articles[i].title)
+          // gathering headlines, headline descriptions, and urls for display in news columns
+          titleAndUrl.push({
+            headlineText: response.articles[i].title,
+            description: response.articles[i].description,
+            url: response.articles[i].url
           })
-          this.props.dispatch(
-            populateState(siteArray[j], titleAndUrl, filteredArray)
-          )
-          // set timeout prevents the jittery rerendering of the word cloud after a second search
-          setTimeout(() => {
-            this.props.dispatch(loading())
-          }, 100)
+        }
+        // combines all descriptions for word cloud
+        let joinedDescriptions = descriptionArray.join('').split(' ')
+        // counts frequency of words
+        const wordCloudArray = new Freq(joinedDescriptions)
+        // word cloud configuration
+        let result = wordCloudArray.list()
+        let initializedArray = []
+        for (let b = 0; b < result.length; b++) {
+          initializedArray.push({
+            text: result[b].word,
+            value: result[b].count
+          })
+        }
+        let wordFilter = [
+          'Fox',
+          'MSNBC',
+          'fox',
+          'cnn',
+          'CNN',
+          '(CNN)',
+          'from',
+          'with',
+          'a',
+          'A',
+          'the',
+          'who',
+          'of',
+          'to',
+          'and',
+          'in',
+          'on',
+          'for'
+        ]
+        // removing common unneeded words from wordcloud
+        var filteredArray = initializedArray.filter(function (el) {
+          return wordFilter.indexOf(el.text) <= -1
         })
+        this.props.dispatch(   
+          // sourceNum, siteSource, headlinesArray, mainWords
+        populateState(sourceArray[i], newsSourceArray[i], titleAndUrl, filteredArray)
+        )
+        // set timeout prevents the jittery rerendering of the word cloud after a second search
+        setTimeout(() => {
+          this.props.dispatch(loading())
+        }, 100)
+      })
     }
   }
-
+  
   render () {
     let formClass
     if (this.props.setInitialSearch) {
@@ -107,10 +107,10 @@ export class Search extends React.Component {
     }
     return (
       <form className={formClass} onSubmit={e => {
-                                        this.searchSent(this.textInput.value, e)
+                                        this.testQuery(this.textInput.value, 'publishedAt', 5, ['cnn', 'msnbc', 'fox'], e)
                                       }}>
         <input
-          value='Putin'
+          value='Trump'
           autoFocus
           className={this.props.size}
           ref={input => {
