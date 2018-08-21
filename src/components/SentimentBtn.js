75f9a9257861de3d84adf18096819f2cb8ddd1f9
Robp773 <robpeterman573@gmail.com>
2018-08-20 21:52:58 -0400

    added responsiveness + icon showing/hiding based on screen size and hover states

--- src/components/SentimentBtn.js
+++ src/components/SentimentBtn.js
@@ -1,66 +1,66 @@
-import React from 'react';
-import './SentimentBtn.css';
-import {updateOpinion} from '../actions';
-import {connect} from 'react-redux';
+import React from 'react'
+import './SentimentBtn.css'
+import { updateOpinion } from '../actions'
+import { connect } from 'react-redux'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-export class SentimentBtn extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            loadingOpinion: false,
-            buttonHidden: false
-        }   
+export class SentimentBtn extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      loadingOpinion: false,
+      buttonHidden: false
     }
-    getSentiment(e){
-        this.setState({loadingOpinion: true})
-        fetch(`https://api.dandelion.eu/datatxt/sent/v1/?token=ad1374871af846789221035b037ed543&url=${this.props.url}`)
-        .then((result)=>{
-            return result.json()
-        })
-        .then((json)=>{
+  }
+  getSentiment (e) {
+    this.setState({loadingOpinion: true})
+    fetch(`https://api.dandelion.eu/datatxt/sent/v1/?token=ad1374871af846789221035b037ed543&url=${this.props.url}`)
+      .then((result) => {
+        return result.json()
+      })
+      .then((json) => {
         this.props.dispatch(updateOpinion(this.props.index, json.sentiment.type, this.props.site))
-            this.setState({loadingOpinion: false, buttonHidden: true})
-})
-    }
+        this.setState({loadingOpinion: false, buttonHidden: true})
+      })
+  }
 
-    render(){
-        let colorClass;
-        let opinion;
-            if(this.state.loadingOpinion){
-                return(<div className="loading">
-                <div className="loading-bar"></div>
-                <div className="loading-bar"></div>
-                <div className="loading-bar"></div>
-                <div className="loading-bar"></div>
+  render () {
+    let colorClass
+    let opinion
+    if (this.state.loadingOpinion) {
+      return (<div className='loading'>
+                <div className='loading-bar'></div>
+                <div className='loading-bar'></div>
+                <div className='loading-bar'></div>
+                <div className='loading-bar'></div>
               </div>)
-            }
-            else if(this.state.loadingOpinion === false & this.state.buttonHidden){
-                if(this.props.opinion === 'negative'){
-                    colorClass = 'red';
-                    opinion = 'Negative';
-                }
-                else if(this.props.opinion === 'neutral'){
-                    colorClass = 'orange';
-                    opinion = 'Neutral';
+    }
+    else if (this.state.loadingOpinion === false & this.state.buttonHidden) {
+      if (this.props.opinion === 'negative') {
+        colorClass = 'red'
+        opinion = 'Negative'
+      }
+      else if (this.props.opinion === 'neutral') {
+        colorClass = 'orange'
+        opinion = 'Neutral'
+      }else {
+        colorClass = 'green'
+        opinion = 'Positive'
+      }
+      return (
+        <h4 className={`opinion ${colorClass}`}>{opinion} Sentiment</h4>
+      )
+    }else {
+      return (
+        <div className='sentimentParent'>
+          <button onClick={(e) => {
+                             this.getSentiment(e)}} className='hideShow sentimentBtn'>
+            <FontAwesomeIcon icon='calculator' />
+          </button>
+        </div>
+      )
+    }
+  }
+}
 
-                }
-                else {
-                    colorClass = 'green';
-                    opinion = 'Positive';
-                }
-                return (
-                    <h4 className={`${colorClass} opinionResult`}>{opinion}</h4>
-                )
-            }
-            else{
-                return(
-                        <div>
-                            <h4 className='opinionResult'>{this.props.opinion}</h4>
-                            <button onClick={(e)=>{this.getSentiment(e)}} className='hideShow sentimentBtn'>Calculate Opinion</button>
-                        </div>
-                    )
-                }    
-            }
-         }
-
-    export default connect()(SentimentBtn)
+export default connect()(SentimentBtn)
--- src/components/SingleHeadline.js
+++ src/components/SingleHeadline.js
@@ -1,46 +1,56 @@
 import React from 'react'
-import SentimentBtn from './SentimentBtn';
-import './SingleHeadline.css';
+import SentimentBtn from './SentimentBtn'
+import './SingleHeadline.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-export default class SingleHeadline extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            expanded: false
-        }
+export default class SingleHeadline extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      expanded: false
     }
+  }
 
-    onExpandClick(){
-     this.setState({expanded: !this.state.expanded})
+  onExpandClick () {
+    this.setState({expanded: !this.state.expanded})
+  }
+
+  render () {
+    let headlineCSS, mobileVis, mobileBtn
+    if (this.state.expanded) {
+      headlineCSS = 'expanded'
+      mobileVis = 'mobileShowBtns'
+      mobileBtn = <FontAwesomeIcon icon='chevron-circle-up' />
     }
-    render(){            
-        let downSymbol = <i className="fas fa-chevron-circle-down"></i>
-        let upSymbol = <i className="fas fa-chevron-up"></i> 
-        let headlineCSS;
-        if(this.state.expanded){
-            headlineCSS = 'expanded'
-        }
-        else if(!this.state.expanded){
-            headlineCSS = 'headline';
-        }
-            return(
-                    <div className={headlineCSS}>
-                        <div className='linkContainer'>
-                            <a  className='headlineLink' href={this.props.headlineUrl}>
-                                {this.props.headlineText}
-                                <div className='borderDiv'></div>            
-                            </a>
-                            <button className='mobileExpandBtn' onClick={()=>{this.onExpandClick()}}>
-                            {/* not working */}
-                               {this.state.expanded ? upSymbol: downSymbol }
-                            </button>
-                        </div>
-                        <SentimentBtn site = {this.props.site} index={this.props.index} url={this.props.headlineUrl} opinion={this.props.opinion}/> 
-                        <div className='hideShow description'>{this.props.description}</div> 
-                    </div>
-            )   
+    else if (!this.state.expanded) {
+      headlineCSS = 'headline'
+      mobileVis = 'mobileHideBtns'
+      mobileBtn = <FontAwesomeIcon icon='chevron-circle-down' />
     }
+    return (
+      <div className={headlineCSS}>
+        <div className='linkContainer'>
+          <h4 className='headlineLink'>{this.props.headlineText} <div className='borderDiv'></div></h4>
+          <button className='mobileExpandBtn' onClick={() => {
+                                                         this.onExpandClick()}}>
+            {mobileBtn}
+          </button>
+          <div className={mobileVis}>
+            <a href={this.props.headlineUrl} target='#' className={`hideShow headlineLinkBtn`}>
+              <FontAwesomeIcon icon='link' />
+            </a>
+            <SentimentBtn
+              site={this.props.site}
+              index={this.props.index}
+              url={this.props.headlineUrl}
+              opinion={this.props.opinion} />
+          </div>
+        </div>
+        <div className='hideShow description'>
+          {this.props.description}
+        </div>
+      </div>
+    )
+  }
 
 }
-
-
