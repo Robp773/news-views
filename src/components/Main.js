27ec9f601ea86c58c0731788fb802f5781f5a1c3
Robp773 <robpeterman573@gmail.com>
2018-08-21 16:00:19 -0400

    added sourceNum prop + changed other props to match new reducer keys

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,27 @@
-import "./Main.css";
-import React from "react";
-import NewsColumn from "./NewsColumn";
-import Search from "./Search";
-import { connect } from "react-redux";
+import './Main.css'
+import React from 'react'
+import NewsColumn from './NewsColumn'
+import Search from './Search'
+import { connect } from 'react-redux'
 
 export class Main extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor (props) {
+    super(props)
     // tracks whether the first search has been made to make the search input change
     // position and get smaller to make room for rest of interface.
-    this.state = { initialSearch: false };
-    this.initialSearch = this.initialSearch.bind(this);
+    this.state = { initialSearch: false }
+    this.initialSearch = this.initialSearch.bind(this)
   }
 
-  initialSearch() {
-    this.setState({ initialSearch: true });
+  initialSearch () {
+    this.setState({ initialSearch: true })
   }
 
-  render() {
+  render () {
     if (this.props.fullState.loading) {
       return (
-        <div className="overlay-loader">
-          <div className="loader">
+        <div className='overlay-loader'>
+          <div className='loader'>
             <div />
             <div />
             <div />
@@ -31,59 +31,48 @@ export class Main extends React.Component {
             <div />
           </div>
         </div>
-      );
+      )
     }
     if (!this.state.initialSearch) {
       return (
-        <div className="main ">
+        <div className='main '>
           <h1>News Views</h1>
-          <Search
-            setInitialSearch={this.initialSearch}
-            size="initialSize"
-            fullState={this.props.fullState}
-          />
+          <Search setInitialSearch={this.initialSearch} size='initialSize' fullState={this.props.fullState} />
         </div>
-      );
+      )
     }
     // following searches
     return (
-      <div className="main">
+      <div className='main'>
         <h1>News Views</h1>
-        <Search
-          setInitialSearch={false}
-          size="searchedSize"
-          fullState={this.props.fullState}
-        />
-        <div className="columnParent">
+        <Search setInitialSearch={false} size='searchedSize' fullState={this.props.fullState} />
+        <div className='columnParent'>
           <NewsColumn
+            sourceNum='sourceOne'
             loading={this.props.fullState.loading}
-            site="MSNBC"
-            siteUrl="msnbc"
-            stateObj={this.props.msnbc}
-          />
+            site={this.props.fullState['sourceOne'].site}
+            siteUrl={this.props.fullState['sourceOne'].site}
+            stateObj={this.props.fullState['sourceOne']} />
           <NewsColumn
+            sourceNum='sourceTwo'
             loading={this.props.fullState.loading}
-            site="CNN"
-            siteUrl="cnn"
-            stateObj={this.props.cnn}
-          />
+            site={this.props.fullState['sourceTwo'].site}
+            siteUrl={this.props.fullState['sourceTwo'].site}
+            stateObj={this.props.fullState['sourceTwo']} />
           <NewsColumn
+            sourceNum='sourceThree'
             loading={this.props.fullState.loading}
-            site="FOX"
-            siteUrl="fox-news"
-            stateObj={this.props.fox}
-          />
+            site={this.props.fullState['sourceThree'].site}
+            siteUrl={this.props.fullState['sourceThree'].site}
+            stateObj={this.props.fullState['sourceThree']} />
         </div>
       </div>
-    );
+    )
   }
 }
 
 const mapStateToProps = state => ({
-  fullState: state,
-  msnbc: state.msnbc,
-  cnn: state.cnn,
-  fox: state.fox
-});
+  fullState: state
+})
 
-export default connect(mapStateToProps)(Main);
+export default connect(mapStateToProps)(Main)
