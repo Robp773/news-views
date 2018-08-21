26dbfd9d54ca92027e8a8a1273b56ff4ae0a2af4
Robp773 <robpeterman573@gmail.com>
2018-08-21 15:59:04 -0400

    added edit button to eventually be used for editing the source of news columns

--- src/components/ColumnHeader.js
+++ src/components/ColumnHeader.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './ColumnHeader.css'
 import WordCloud from 'react-d3-cloud'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default class ColumnHeader extends React.Component {
 
@@ -13,20 +14,9 @@ export default class ColumnHeader extends React.Component {
   }
 
   render () {
-    let headerImage
-    if (this.props.site === 'MSNBC') {
-      headerImage = 'https://www.charismanews.com/images/stories/2016/06/MSNBC-Logo.jpg'
-    }
-    else if (this.props.site === 'CNN') {
-      headerImage = 'http://www.stickpng.com/assets/images/5842ab75a6515b1e0ad75b0b.png'
-    }else {
-      headerImage = 'http://ncrius.org/wp-content/uploads/2017/02/FOX-NEWS-864x445.png'
-    }
     const data = this.props.mainWords
     const fontSizeMapper = word => word.value * 15
-
     const rotate = word => 0
-
     let wordCluster = <WordCloud
                         width={300}
                         height={200}
@@ -40,10 +30,13 @@ export default class ColumnHeader extends React.Component {
     }
     return (
       <div className='header'>
-        <h2>
-            <img className='headerImg' alt={`${this.props.site} logo`} src={`${headerImage}`}/></h2>
-            {noResults}
-            {wordCluster}
+        <h2 className='newsHeaderTitle'>{this.props.site}
+          <button className='editSourceBtn'>
+            <FontAwesomeIcon icon='edit' />
+          </button>
+        </h2>
+        {noResults}
+        {wordCluster}
       </div>
     )}
 
