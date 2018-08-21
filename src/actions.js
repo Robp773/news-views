afea3cdb09b913e98add07afe2dc2fdc2a527f4b
Robp773 <robpeterman573@gmail.com>
2018-08-21 15:58:26 -0400

    changed initial state to allow users to eventually choose thier own source + updated actions to match changes

--- src/actions.js
+++ src/actions.js
@@ -1,27 +1,28 @@
-export const changeVis = (site) =>({
-    type: 'CHANGE_VIS',
-    site: site
-});
-
-export const populateState = (site, headlinesArray, mainWords)=>({
-    type: "POPULATE_STATE",
-    site: site,
-    headlinesArray: headlinesArray,
-    mainWords: mainWords,
-});
-
-export const updateOpinion = (index, opinion, site)=>({
-    type: 'UPDATE_OPINION',
-    index: index,
-    opinion: opinion,
-    site: site
+export const changeVis = (site) => ({
+  type: 'CHANGE_VIS',
+  site: site
 })
 
-export const loading = () =>({
-    type: 'LOADING'
+export const populateState = (sourceNum, siteSource, headlinesArray, mainWords) => ({
+  type: 'POPULATE_STATE',
+  sourceNum: sourceNum,
+  siteSource: siteSource,
+  headlinesArray: headlinesArray,
+  mainWords: mainWords
 })
 
-export const emptyColumn = (site) =>({
-    type: 'EMPTY',
-    site: site
+export const updateOpinion = (index, opinion, sourceNum) => ({
+  type: 'UPDATE_OPINION',
+  index: index,
+  opinion: opinion,
+  sourceNum: sourceNum
+})
+
+export const loading = () => ({
+  type: 'LOADING'
+})
+
+export const emptyColumn = (site) => ({
+  type: 'EMPTY',
+  site: site
 })
--- src/reducer.js
+++ src/reducer.js
@@ -1,56 +1,57 @@
-
-
 let initialState = {
-    loading: false,
-    msnbc: {
-        site: 'msnbc',
-        mobileVis: false,
-        mainWords: [],
-        headlines: [],
-    },
-    cnn: {
-        site: 'cnn',
-        mobileVis: true,
-        mainWords: [],
-        headlines: [],
+  loading: false,
+  sourceOne: {
+    site: 'msnbc',
+    mobileVis: false,
+    mainWords: [],
+    headlines: []
+  },
+  sourceTwo: {
+    site: 'cnn',
+    mobileVis: true,
+    mainWords: [],
+    headlines: []
 
-    },
-    fox: {
-        site: 'fox',
-        mobileVis: false,
-        mainWords: [],
-        headlines: [],
-    }
+  },
+  sourceThree: {
+    site: 'fox',
+    mobileVis: false,
+    mainWords: [],
+    headlines: []
+  }
 }
 
-export const reducer =  (state = initialState, action) =>{
-    if(action.type === 'CHANGE_VIS'){
-        let siteArray = ['msnbc', 'cnn', 'fox']
-        for(let i =0; i<siteArray.length; i++){
-            if(siteArray[i] === action.site){
-                siteArray.splice(i, 1)
-            }
-        }
-       return Object.assign({}, state, state[action.site].mobileVis = true, 
-            state[siteArray[0]].mobileVis = false, state[siteArray[1]].mobileVis = false
-        )
+export const reducer = (state = initialState , action) => {
+  if (action.type === 'CHANGE_VIS') {
+    let siteArray = ['msnbc', 'cnn', 'fox']
+    for (let i = 0; i < siteArray.length; i++) {
+      if (siteArray[i] === action.site) {
+        siteArray.splice(i, 1)
+      }
     }
+    return Object.assign({}, state, state[action.site].mobileVis = true,
+      state[siteArray[0]].mobileVis = false, state[siteArray[1]].mobileVis = false
+    )
+  }
 
-    if(action.type === 'POPULATE_STATE'){        
-        return Object.assign({}, state, state[action.site].headlines = action.headlinesArray, 
-            state[action.site].mainWords = action.mainWords)
-    }
+  if (action.type === 'POPULATE_STATE') {
+    console.log(action)
+    return Object.assign({}, state,
+      state[action.sourceNum].site = action.siteSource,
+      state[action.sourceNum].headlines = action.headlinesArray,
+      state[action.sourceNum].mainWords = action.mainWords)
+  }
 
-    if(action.type === 'UPDATE_OPINION'){
-        let lowerCaseSite = (action.site).toLowerCase();
-        let index = action.index
-        return Object.assign({}, state, ...state[lowerCaseSite].headlines[index].opinion = action.opinion)
-    }
+  if (action.type === 'UPDATE_OPINION') {
+    
+    let index = action.index
+    return Object.assign({}, state, ...state[action.sourceNum].headlines[index].opinion = action.opinion)
+  }
 
-    if(action.type === 'LOADING'){
-        return Object.assign({}, state, state.loading = !state.loading)
-    }
-    if(action.type === 'EMPTY'){
-        return Object.assign({}, state, state[action.site].headlines=[], state[action.site].mainWords = [])}
-return state;
+  if (action.type === 'LOADING') {
+    return Object.assign({}, state, state.loading = !state.loading)
+  }
+  if (action.type === 'EMPTY') {
+    return Object.assign({}, state, state[action.site].headlines = [], state[action.site].mainWords = [])}
+  return state
 }
