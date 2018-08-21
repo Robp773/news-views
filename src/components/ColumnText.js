855e3c9545563a0c71691f7bc0cb6e745f04ca2a
Robp773 <robpeterman573@gmail.com>
2018-08-21 15:59:39 -0400

    added source num prop to allow actions to select the right part of reducer state with updated reducer keys

--- src/components/ColumnText.js
+++ src/components/ColumnText.js
@@ -1,28 +1,27 @@
-import React from 'react';
-import './ColumnText.css';
-import SingleHeadline from './SingleHeadline';
+import React from 'react'
+import './ColumnText.css'
+import SingleHeadline from './SingleHeadline'
 
-export default class ColumnText extends React.Component{
-    render(){
-        let headlines = this.props.headlines;
-        let headlinesArray = [];
-        for(let i=0;i<headlines.length;i++){
-        
-        headlinesArray.push(
-        <SingleHeadline 
-            key={`${this.props.site}${i}`} 
-            site={this.props.site}
-            headlineText={headlines[i].headlineText}
-            headlineUrl={headlines[i].url}
-            opinion={headlines[i].opinion}
-            description={headlines[i].description}
-            index={i}
-        />
-        )}
-        return (
-                <div>{headlinesArray}
-                </div>
-               ) 
-            }
+export default class ColumnText extends React.Component {
+  render () {
+    let headlines = this.props.headlines
+    let headlinesArray = []
+    for (let i = 0;i < headlines.length;i++) {
+      headlinesArray.push(
+        <SingleHeadline
+          sourceNum={this.props.sourceNum}
+          key={`${this.props.site}${i}`}
+          site={this.props.site}
+          headlineText={headlines[i].headlineText}
+          headlineUrl={headlines[i].url}
+          opinion={headlines[i].opinion}
+          description={headlines[i].description}
+          index={i} />
+      )}
+    return (
+      <div>
+        {headlinesArray}
+      </div>
+    )
+  }
 }
-
